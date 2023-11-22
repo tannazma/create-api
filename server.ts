8c757a5665596d7898338dc1c75e05f60a34ce20
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-22 12:25:57 +0100

    create get request for geting users data

--- server.ts
+++ server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import user from "./prisma/data/user.json";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send({ message: "Here is server" });
+app.get("/", async (req, res) => {
+  const allUsers = await prisma.user.findMany();
+
+  res.send(allUsers);
 });
 
 app.listen(port, () => {
