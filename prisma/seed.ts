c3af4625ffbfa3717e4b6b5a7c1362312ec2dd27
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-22 23:31:29 +0100

    create pet, food and owner via json files in database in seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,7 @@
 import user from "./data/user.json";
+import petData from "./data/pets.json";
+import ownerData from "./data/owner.json";
+import foodData from "./data/food.json";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
@@ -8,6 +11,28 @@ const seed = async () => {
     const thisUser = user[i];
     await prisma.user.create({
       data: thisUser,
+    });
+  }
+  for (let i = 0; i < ownerData.length; i++) {
+    const thisOwner = ownerData[i];
+    await prisma.owner.create({
+      data: thisOwner,
+    });
+  }
+
+  // Seed every food
+  for (let i = 0; i < foodData.length; i++) {
+    const thisFood = foodData[i];
+    await prisma.food.create({
+      data: thisFood,
+    });
+  }
+
+  // Seed every pet
+  for (let i = 0; i < petData.length; i++) {
+    const thisPet = petData[i];
+    await prisma.pet.create({
+      data: thisPet,
     });
   }
 };
