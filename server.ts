e4410a525759313a73a50d79cfbfe0524dab1a4a
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-22 16:55:23 +0100

    an endpoint to return the user with an id equal to the :id in the route

--- server.ts
+++ server.ts
@@ -18,6 +18,25 @@ app.get("/users", async (req, res) => {
   res.send(allUsers);
 });
 
+// an endpoint to return the user with an id equal to the :id in the route
+app.get("/users/:id", async (req, res) => {
+  //   const idFromParams = req.params.id;
+  const idAsNumber = Number(req.params.id);
+  if (isNaN(idAsNumber)) {
+    res.status(400).send({ message: "id should be a number" });
+    return; //empty return means that we are done
+  }
+  const oneUser = await prisma.user.findUnique({
+    where: {
+      id: idAsNumber,
+    },
+  });
+  if (!oneUser) {
+    res.status(404).send({ message: "Could not find that user" });
+  }
+  res.send(oneUser);
+});
+
 app.listen(port, () => {
   console.log(`⚡ Server listening on port: ${port}`);
 });
